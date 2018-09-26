009db50873325d6aa7d48a9427c4e13481209eae
Łukasz Derek <lukasz.derek@gmail.com>
2018-09-26 18:07:49 +0200

    feature/FZ9SS-36 refactored data

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -3,18 +3,18 @@ import {CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
 
 const DashBoard = () => {
     var data = [
-        {name: 'Styczeń', KONKURENCJA: 10, MY: 10},
-        {name: 'Luty', KONKURENCJA: 18, MY: 23, amt: 2210},
-        {name: 'Marzec', KONKURENCJA: 23, MY: 28, amt: 2290},
-        {name: 'Kwiecień', KONKURENCJA: 29, MY: 35, amt: 2000},
-        {name: 'Maj', KONKURENCJA: 35, MY: 42, amt: 2181},
-        {name: 'Czerwiec', KONKURENCJA: 42, MY: 50, amt: 2500},
-        {name: 'Lipiec', KONKURENCJA: 49, MY: 61, amt: 2100},
-        {name: 'Sierpień', KONKURENCJA: 53, MY: 72, amt: 2100},
-        {name: 'Wrzesień', KONKURENCJA: 61, MY: 80, amt: 2100},
-        {name: 'Październik', KONKURENCJA: 63, MY: 89, amt: 2100},
-        {name: 'Listopad', KONKURENCJA: 69, MY: 91, amt: 2100},
-        {name: 'Grudzień', KONKURENCJA: 72, MY: 99, amt: 2100},
+        {name: 'Styczeń', KONKURENCJA: 10, MY: 15},
+        {name: 'Luty', KONKURENCJA: 13, MY: 23},
+        {name: 'Marzec', KONKURENCJA: 17, MY: 28},
+        {name: 'Kwiecień', KONKURENCJA: 22, MY: 35},
+        {name: 'Maj', KONKURENCJA: 29, MY: 42},
+        {name: 'Czerwiec', KONKURENCJA: 35, MY: 50},
+        {name: 'Lipiec', KONKURENCJA: 41, MY: 61},
+        {name: 'Sierpień', KONKURENCJA: 49, MY: 72},
+        {name: 'Wrzesień', KONKURENCJA: 52, MY: 80},
+        {name: 'Październik', KONKURENCJA: 60, MY: 89},
+        {name: 'Listopad', KONKURENCJA: 63, MY: 91},
+        {name: 'Grudzień', KONKURENCJA: 68, MY: 99},
     ];
     return(
         <LineChart width={500} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
