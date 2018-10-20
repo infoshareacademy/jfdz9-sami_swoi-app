5524b07c3ab96dfb621ee9e020915924270f8664
zbooyx <zbooyx@gmail.com>
2018-10-20 20:58:52 +0200

    popup for single offer

--- src/components/SingleOffer/SingleOffer.js
+++ src/components/SingleOffer/SingleOffer.js
@@ -9,85 +9,54 @@ import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
-class SingleOffer extends Component {
-
-    state = {
-        filteredJobOffers: []
-    };
-
-    componentDidMount() {
-        fetch('/Data/job_offers.json')
-            .then(response => response.json())
-            .then(job_offers => {
-
-
-                this.setState({
-                    filteredJobOffers: job_offers.slice(1, 2)
-                })
-
-            })
-    }
-
-
-    render() {
-        return (
-
+const SingleOffer = ({offer}) =>
+        (
             <div className='ui basic segment'>
-                {
-                    this.state.filteredJobOffers.map(offer => {
-                        return (
-                            <Card key={offer.id}>
-                                <CardContent>
-                                    <Typography component="h2" variant="headline">
-                                        Wakat: {offer.title}
-                                    </Typography>
-                                    <Typography component="p">
-                                        Dodano: {offer.createdAt}
-                                    </Typography>
+                <Card>
+                    <CardContent>
+                        <Typography component="h2" variant="headline">
+                            Wakat: {offer.title}
+                        </Typography>
+                        <Typography component="p">
+                            Dodano: {offer.createdAt}
+                        </Typography>
 
-                                    <Typography component="p">
-                                       Firma: {offer.company.name}
-                                    </Typography>
+                        <Typography component="p">
+                           Firma: {offer.company.name}
+                        </Typography>
 
-                                    <Typography component="p">
-                                        Minimalne wynagrodzenie: {offer.salary.min}
-                                    </Typography>
+                        <Typography component="p">
+                            Minimalne wynagrodzenie: {offer.salary.min}
+                        </Typography>
 
-                                    <Typography component="p">
-                                        Opis: {offer.description}
-                                    </Typography>
+                        <Typography component="p">
+                            Opis: {offer.description}
+                        </Typography>
 
 
-                                </CardContent>
+                    </CardContent>
 
-                                <CardActions>
-                                    <IconButton aria-label="Add to favorites">
-                                        <FavoriteIcon/>
-                                    </IconButton>
-                                    <IconButton aria-label="Share">
-                                        <ShareIcon/>
-                                    </IconButton>
-                                    <IconButton
-                                        // className={classnames(classes.expand, {
-                                        //     [classes.expandOpen]: this.state.expanded,
-                                        // })}
-                                        // onClick={this.handleExpandClick}
-                                        // aria-expanded={this.state.expanded}
-                                        aria-label="Show more"
-                                    >
-                                        <ExpandMoreIcon/>
-                                    </IconButton>
-                                </CardActions>
-                            </Card>
-                        )
-                    })
-                }
-
+                    {/*<CardActions>*/}
+                        {/*<IconButton aria-label="Add to favorites">*/}
+                            {/*<FavoriteIcon/>*/}
+                        {/*</IconButton>*/}
+                        {/*<IconButton aria-label="Share">*/}
+                            {/*<ShareIcon/>*/}
+                        {/*</IconButton>*/}
+                        {/*<IconButton*/}
+                            {/*// className={classnames(classes.expand, {*/}
+                            {/*//     [classes.expandOpen]: this.state.expanded,*/}
+                            {/*// })}*/}
+                            {/*// onClick={this.handleExpandClick}*/}
+                            {/*// aria-expanded={this.state.expanded}*/}
+                            {/*aria-label="Show more"*/}
+                        {/*>*/}
+                            {/*<ExpandMoreIcon/>*/}
+                        {/*</IconButton>*/}
+                    {/*</CardActions>*/}
+                </Card>
             </div>
-
-        )
-    }
-}
+        );
 
 export default SingleOffer;
 
